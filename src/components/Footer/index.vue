05ea3ae8ed72975aa5e1eb1cc9c880c71d599117
wujiajing <445476933@qq.com>
2020-11-27 16:23:44 +0800

    feat:项目大框

---
+++ src/components/Footer/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="footer">
+    Footer.....
+    <p>footer 哟</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style lang="less" scoped>
+.footer {
+  background-color: #ddd;
+  p {
+    color: hotpink;
+    font-size: 20px;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,30 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+
+import Home from '../views/Home';
+import Login from '../views/Login';
+import Register from '../views/Register';
+import Search from '../views/Search';
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-];
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes,
+export default new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: Home,
+    },
+    {
+      path: '/login',
+      component: Login,
+    },
+    {
+      path: '/register',
+      component: Register,
+    },
+    {
+      path: '/search',
+      component: Search,
+    },
+  ],
 });
-
-export default router;
