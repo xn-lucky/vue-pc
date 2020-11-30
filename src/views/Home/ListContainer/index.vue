0eac0ac02e30411b7b909eef1140ac5e33a901b2
xn-lucky <isxunan@126.com>
2020-12-01 00:35:45 +0800

    feat:home右侧样式提交

--- src/views/Home/ListContainer/index.vue
+++ src/views/Home/ListContainer/index.vue
@@ -19,8 +19,23 @@
           <li><em>[特惠]</em>备战开学季 全民半价购数码</li>
         </ul>
       </div>
-      <div class="listcontainer-right-middle"></div>
-      <div class="listcontainer-right-bottom"></div>
+      <ul class="listcontainer-right-middle">
+        <li class="listcontainer-right-middle-item"><i></i><span>话费</span></li>
+        <li class="listcontainer-right-middle-item"><i></i><span>机票</span></li>
+        <li class="listcontainer-right-middle-item"><i></i><span>电影票</span></li>
+        <li class="listcontainer-right-middle-item"><i></i><span>游戏</span></li>
+        <li class="listcontainer-right-middle-item"><i></i><span>彩票</span></li>
+        <li class="listcontainer-right-middle-item"><i></i><span>加油站</span></li>
+        <li class="listcontainer-right-middle-item"><i></i><span>酒店</span></li>
+        <li class="listcontainer-right-middle-item"><i></i><span>火车票</span></li>
+        <li class="listcontainer-right-middle-item"><i></i><span>众筹</span></li>
+        <li class="listcontainer-right-middle-item"><i></i><span>理财</span></li>
+        <li class="listcontainer-right-middle-item"><i></i><span>礼品卡</span></li>
+        <li class="listcontainer-right-middle-item"><i></i><span>白条</span></li>
+      </ul>
+      <div class="listcontainer-right-bottom">
+        <img src="../images/home/ad1.png" alt="">
+      </div>
     </div>
   </div>
 </template>
@@ -99,5 +114,71 @@ export default {
 }
 .listcontainer-right-list em {
   font-weight: 700;
+}
+
+.listcontainer-right-middle{
+  width: 250px;
+  height: 195px;
+  border-right: 1px solid #e4e4e4;
+  display: flex;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+}
+.listcontainer-right-middle-item{
+  width: 61px;
+  height: 64px;
+  border-left: 1px solid #e4e4e4;
+  border-bottom: 1px solid #e4e4e4;
+  display: flex;
+  flex-direction: column;
+}
+.listcontainer-right-middle-item i{
+  width: 61px;
+  height: 40px;
+  background: url('../images/home/icons.png');
+}
+.listcontainer-right-middle .listcontainer-right-middle-item:nth-child(2) i{
+  background-position: -64px 0px;
+}
+.listcontainer-right-middle .listcontainer-right-middle-item:nth-child(3) i{
+  background-position: -126px 0px;
+}
+.listcontainer-right-middle .listcontainer-right-middle-item:nth-child(4) i{
+  background-position: -190px 0px;
+}
+.listcontainer-right-middle .listcontainer-right-middle-item:nth-child(5) i{
+  background-position: -2px -71px;
+}
+.listcontainer-right-middle .listcontainer-right-middle-item:nth-child(6) i{
+  background-position: -63px -71px;
+}
+.listcontainer-right-middle .listcontainer-right-middle-item:nth-child(7) i{
+  background-position: -126px -71px;
+}
+.listcontainer-right-middle .listcontainer-right-middle-item:nth-child(8) i{
+ background-position: -193px -71px;
+}
+.listcontainer-right-middle .listcontainer-right-middle-item:nth-child(9) i{
+     background-position: 0px -141px;
+}
+.listcontainer-right-middle .listcontainer-right-middle-item:nth-child(10) i{
+  background-position: -65px -144px;
+}
+.listcontainer-right-middle .listcontainer-right-middle-item:nth-child(11) i{
+ background-position: -126px -141px;
+}
+.listcontainer-right-middle .listcontainer-right-middle-item:nth-child(12) i{
+ background-position: -193px -141px;
+}
+.listcontainer-right-middle-item span{
+  width: 61px;
+  height: 20px;
+  line-height: 20px;
+  text-align: center;
+}
+.listcontainer-right-bottom{
+  width: 250px;
+  height: 75px;
+  margin-top: 5px;
 }
 </style>
